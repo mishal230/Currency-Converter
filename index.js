acb763ef7cc50e50db37dde034966c44a68483f3
mishalzubair <160513180+mishal230@users.noreply.github.com>
2024-05-05 20:51:00 +0500

    simple currency converter

---
+++ index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.92,
+    GBP: 0.79,
+    INR: 83.38,
+    PKR: 280
+};
+//async function main() {
+let answer = await inquirer.prompt([{
+        name: "amount",
+        type: "number",
+        message: "Enter the Amount you Want to Convert:"
+    },
+    { name: "currencyFrom",
+        type: "list",
+        message: "Enter the Currency to Convert from:",
+        choices: ["PKR", "USD", "EUR", "GBP", "INR"]
+    },
+    {
+        name: "currencyTo",
+        type: "list",
+        message: "Enter the Currency to Convert to:",
+        choices: ["PKR", "USD", "EUR", "GBP", "INR"]
+    }
+]);
+let fromAmount = currency[answer.currencyFrom];
+let toAmount = currency[answer.currencyTo];
+let amount = answer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(`convertedAmount: ${convertedAmount.toFixed(2)} ${answer.currencyTo}`);
